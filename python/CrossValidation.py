fixed main.py regression; data load preprocessing to remove empty rows; exit condition for LBFGS; masking helper function; BROKEN CrossValidation

python/CrossValidation.py
@@ -8,7 +8,6 @@ from sklearn.cross_validation import KFold
 import solvers
 import util
 from c_extensions.simplex_projection import simplex_projection
-from projection import pysimplex_projection
 # from projection import pysimplex_projection
 import BB, LBFGS, DORE
 import config as c
@@ -16,12 +15,14 @@ from main import parser
 
 class CrossValidation:
 
-    def __init__(self,k=3,f=None,solver=None,var=None,iter=200):
+    def __init__(self,k=3,f=None,solver=None,var=None,iter=200,noise=None):
         self.f=f
         self.solver=solver
         self.var = var
         self.iter = iter
+        self.noise = noise
         self.k=k
+
         self.setup()
         self.kf = KFold(self.n,n_folds=k, indices=True)
         self.iters = [None]*k
@@ -36,13 +37,20 @@ class CrossValidation:
 
     def setup(self):
         # load data
-        self.A, self.b, self.N, self.block_sizes, x_true=util.load_data(self.f)
+        self.A,self.b,self.N,self.block_sizes,self.x_true=util.load_data(self.f)
         self.NT = self.N.T.tocsr()
 
-        self.n = np.size(self.b)
-        self.x_true = np.squeeze(np.array(x_true))
+        # Assumption: noise is proportional to link volume
+        if self.noise:
+            self.b_true = self.b
+            delta = 2*(np.random.random_sample(self.b.shape)-0.5)*self.b*self.noise
+            self.b = self.b + delta
 
-        self.x0 = np.array(util.block_e(self.block_sizes - 1, self.block_sizes))
+        self.n = np.size(self.b)
+
+        self.x0 = np.array(util.block_e(self.block_sizes-1, self.block_sizes))
+        # self.x0 = self.x_true
+
         logging.debug("Blocks: %s" % self.block_sizes.shape)
 
         self.options = { 'max_iter': self.iter,
@@ -92,7 +100,6 @@ class CrossValidation:
             if self.solver == 'LBFGS':
                 LBFGS.solve(self.z0+1, f, nabla_f, solvers.stopping, log=log,
                         proj=self.proj,options=self.options)
-                logging.debug("Took %s time" % str(np.sum(times)))
             elif self.solver == 'BB':
                 BB.solve(self.z0,f,nabla_f,solvers.stopping,log=log,
                         proj=self.proj,options=self.options)
@@ -107,7 +114,8 @@ class CrossValidation:
                         lambda b: self.N.T.dot(A_dore.T.dot(b)), 
                         target_dore,proj=self.proj,log=log,options=self.options)
                 A_dore = None
-            logging.debug('[%d] Stopping %s solver...' % (i,self.solver))
+            logging.debug('[%d] Stopping %s solver... %s' % \
+                    (i,self.solver,str(np.sum(times))))
 
             self.iters[i] = iters
             self.times[i] = times
@@ -117,8 +125,10 @@ class CrossValidation:
     # Post process intermediate states of runs
     def post_process(self):
         self.init_metrics()
-        self.mean_times = np.mean(np.array([np.cumsum(self.times[i]) for i in \
-                range(self.k)]),axis=0)
+
+        self.mean_times = util.mask(self.times).cumsum(axis=0).mean(axis=1)
+        # self.mean_times = np.mean(np.array([np.cumsum(self.times[i]) for i in \
+        #         range(self.k)]),axis=0)
 
         def metrics(A,b,X):
             d = X.shape[1]
@@ -262,10 +272,22 @@ class CrossValidation:
         test_metrics = self.test_bin[metric]
         train_metrics = self.train_bin[metric]
 
+        # TODO do this for individual times instead of mean times
+        inds = [len(self.times[i]) for i in range(len(self.times))]
+        if time_max < self.mean_time:
+            for i in range(len(self.times)):
+                times = np.cumsum(self.times[i])
+                for j in range(len(self.times[i])):
+                    if times[j] > time_max:
+                        inds[i] = j-1
+                        break
+        import ipdb
+        ipdb.set_trace()
+            
         ind = len(self.mean_times)-1
-        for i in range(len(self.mean_times)-1,-1,-1):
-            if self.mean_times[i] <= time_max:
-                ind = i
+        for i in range(len(self.mean_times)):
+            if self.mean_times[i] > time_max:
+                ind = i-1
                 break
 
         for j in range(self.nbins+1):
@@ -274,15 +296,27 @@ class CrossValidation:
                     range(self.k) if test_metrics[i][j] != None]
             train_metric = [train_metrics[i][j] for i in \
                     range(self.k) if train_metrics[i][j] != None]
-            y1   = np.mean(test_metric,   axis=0)
-            y2   = np.mean(train_metric,  axis=0)
-            std1 = np.std(test_metric,    axis=0)
-            std2 = np.std(train_metric,   axis=0)
+            y1 = util.mask(test_metric).mean(axis=1)
+            y2 = util.mask(train_metric).mean(axis=1)
+            # y1   = np.mean(test_metric,   axis=0)
+            # y2   = np.mean(train_metric,  axis=0)
+            std1 = util.mask(test_metric).std(axis=1)
+            std2 = util.mask(train_metric).std(axis=1)
+            # std1 = np.std(test_metric,    axis=0)
+            # std2 = np.std(train_metric,   axis=0)
             if j == 0:
+                print x[j], y1[ind]
+                print std1[ind]
+                print time_max
+                print self.mean_times
+                print ind, len(self.iters[0])
+                print self.iters[0][ind]
                 plt.bar(x[j]-1+offset,y1[ind],label='%s-%s (%d iters)' % \
                         (self.solver,self.var,self.iters[0][ind]),width=0.15,
                         color=color,yerr=std1[ind])
             else:
+                if type(y1) == np.float64:
+                    continue
                 plt.bar(x[j]-1+offset,y1[ind],width=0.15,color=color,
                         yerr=std1[ind])
             plt.hold(True)
@@ -332,14 +366,20 @@ if __name__ == "__main__":
     if args.log in c.ACCEPTED_LOG_LEVELS:
         logging.basicConfig(level=eval('logging.'+args.log))
 
-    m = 1 # multiplier
+    e = 0.03
+    k = 3
+    m = 40 # multiplier
 
-    cv1 = CrossValidation(k=3,f=args.file,solver='BB',var='z',iter=10*m)
-    cv2 = CrossValidation(k=3,f=args.file,solver='DORE',var='z',iter=8*m)
-    cv3 = CrossValidation(k=3,f=args.file,solver='LBFGS',var='z',iter=3*m)
+    cv1 = CrossValidation(k=k,f=args.file,noise=e,solver='BB',var='z',iter=20*m)
+    cv2 = CrossValidation(k=k,f=args.file,noise=e,solver='DORE',var='z',iter=12*m)
+    cv3 = CrossValidation(k=k,f=args.file,noise=e,solver='LBFGS',var='z',iter=5*m)
 
-    cvs = [cv3,cv1,cv2]
+    cvs = [cv1,cv2,cv3]
     colors = ['b','m','g']
+    # cvs = [cv1,cv2]
+    # colors = ['b','m']
+    # cvs = [cv3]
+    # colors = ['g']
     for cv in cvs:
         cv.run()
         cv.post_process()
@@ -350,16 +390,13 @@ if __name__ == "__main__":
     [cv.plot_all(subplot=122,color=c) for (cv,c) in zip(cvs,colors)]
 
     # Compute time cap
-    time_max = np.min([cv.mean_times[-1] for cv in cvs])
+    time_max = np.min([cv.mean_time for cv in cvs])
+    # time_max = 0
 
     offsets = [0,1./3,2./3]
     # plt.figure()
     # [cv.plot_bar_bins(color=c,offset=o,time_max=time_max) for \
     #         (cv,c,o) in zip(cvs,colors,offsets)]
-
-    plt.figure()
-    [cv.plot_bar_bins(color=c,offset=o,time_max=time_max,
-        metric='mean_GEH') for (cv,c,o) in zip(cvs,colors,offsets)]
 
     plt.figure()
     [cv.plot_bar_bins(color=c,offset=o,time_max=time_max,
@@ -371,8 +408,8 @@ if __name__ == "__main__":
     [cv.plot_bar_bins(color=c,offset=o,time_max=time_max,
         metric='GEH_under_1') for (cv,c,o) in zip(cvs,colors,offsets)]
 
-    plt.figure()
-    [cv.plot_bar_bins(color=c,offset=o,time_max=time_max,
-        metric='GEH_under_0.5') for (cv,c,o) in zip(cvs,colors,offsets)]
+    # plt.figure()
+    # [cv.plot_bar_bins(color=c,offset=o,time_max=time_max,
+    #     metric='GEH_under_0.5') for (cv,c,o) in zip(cvs,colors,offsets)]
 
     plt.show()

python/LBFGS.py
@@ -17,21 +17,33 @@ def weak_wolfe_ls(x,d,f,nabla_f,proj=lambda x: x, c1=1e-3,c2=0.9):
     proj_x = proj(x)
     nabla_fx = nabla_f(proj_x)
     while not stop:
+        # print t, x
         proj_xtd = proj(x + t*d)
 
         # armijo condition violated
-        if f(proj_xtd) > f(proj_x) + c1 * t * d.dot(nabla_fx):
-            # print 'ar', alpha, beta, t
+        if f(proj_xtd) >= f(proj_x) + c1 * t * d.dot(nabla_fx):
+            # print 'ar', f(proj_xtd),f(proj_x)+c1*t*d.dot(nabla_fx),f(proj_x),alpha,beta,t
+            # print nabla_fx
             beta = t
             t = 0.5 * (alpha + beta)
         # curvature condition violated
         elif d.dot(nabla_f(proj_xtd)) < c2 * d.dot(nabla_fx):
-            # print 'cc', alpha,beta, t
+            # print 'cc', d.dot(nabla_f(proj_xtd)),c2*d.dot(nabla_fx),alpha,beta,t
             alpha = t
-            t = 2 * alpha if beta == float('inf') else 0.5*(alpha+beta)
+            t = 2 * alpha if beta == float('inf') else 0.5 * (alpha + beta)
+            if np.abs(alpha-beta) <= 1e-15:
+                stop = True
         # both conditions pass
         else:
             stop = True
+        
+        if la.norm(t*d) <= 1e-8:
+            stop = True
+
+        if np.abs(alpha-beta) == 0:
+            import ipdb
+            ipdb.set_trace()
+        # print np.abs(alpha-beta)
     return t
 
 # Limited memory BFGS
@@ -62,6 +74,7 @@ def solve(x0, f, nabla_f, stopping, m=10,record_every=5,proj=None, log=None,
     y, s = [np.zeros((n))]*m, [np.zeros((n))]*m
     g_new = nabla_f(x)
     y_new, s_new = g_new, np.ones((n))
+
     rho, rho_new = [0]*m, 1 / (y_new.dot(s_new))
     while not stop:
         i+=1
@@ -83,11 +96,15 @@ def solve(x0, f, nabla_f, stopping, m=10,record_every=5,proj=None, log=None,
         g = g_new
         g_new = nabla_f(x_next)
         y_new = g_new - g 
+        if y_new.dot(s_new) == 0:
+            print "iter=%d, f=%8.5e" % (i,f(x_next))
+            import ipdb
+            ipdb.set_trace()
         rho_new = 1 / (y_new.dot(s_new))
 
         x = x_next
         fx = f(x)
-        stop = stopping(g_new,fx,i,t,options)
+        stop = stopping(g_new,fx,i,t,d=d,options=options)
 
         # Save intermediate state
         if i % record_every == 0:

python/main.py
@@ -33,8 +33,6 @@ def main():
     sio.savemat('fullData.mat', {'A':A,'b':b,'N':block_sizes,'N2':N,
         'x_true':x_true})
 
-    x_true = np.squeeze(np.array(x_true))
-
     # Sample usage
     #P = A.T.dot(A)
     #q = A.T.dot(b)
@@ -43,7 +41,7 @@ def main():
 
     logging.debug("Blocks: %s" % block_sizes.shape)
     x0 = np.array(util.block_e(block_sizes - 1, block_sizes))
-    target = b-A.dot(x0)
+    target = A.dot(x0)-b
 
     options = { 'max_iter': 100,
                 'verbose': 1,
@@ -52,14 +50,12 @@ def main():
     AT = A.T.tocsr()
     NT = N.T.tocsr()
 
-    target = A.dot(x0) - b
-
     f = lambda z: 0.5 * la.norm(A.dot(N.dot(z)) + target)**2
     nabla_f = lambda z: NT.dot(AT.dot(A.dot(N.dot(z)) + target))
 
     def proj(x):
         projected_value = simplex_projection(block_sizes - 1,x)
-        #projected_value = pysimplex_projection(block_sizes - 1,x)
+        # projected_value = pysimplex_projection(block_sizes - 1,x)
         return projected_value
 
     z0 = np.zeros(N.shape[1])

python/util.py
@@ -96,12 +96,9 @@ def load_data(filename):
     else:
         A = data['A']
     A = A.tocsr()
-
-    if data.has_key('b'):
-        b = data['b']
-    else:
-        b = data['f']
-    b = np.squeeze(np.asarray(b))
+    # Remove rows of zeros
+    nnz = [i for i in xrange(A.shape[0]) if A[i,:].nnz > 0]
+    A = sps.lil_matrix(A[nnz,:]).tocsr()
 
     if data.has_key('block_sizes'):
         block_sizes = data['block_sizes']
@@ -112,6 +109,14 @@ def load_data(filename):
         x_true = data['x']
     else:
         x_true = data['real_a']
+    x_true = np.squeeze(np.array(x_true))
+
+    # if data.has_key('b'):
+    #     b = data['b']
+    # else:
+    #     b = data['f']
+    # b = np.squeeze(np.asarray(b))
+    b = A.dot(x_true)
 
     logging.debug('Creating sparse N matrix')
     N = block_sizes_to_N(block_sizes)
@@ -218,6 +223,14 @@ def init_xz(block_sizes, x_true):
     
     return x1,x2,x3,x4,z1,z2,z3,z4
 
+def mask(arr):
+    k = len(arr)
+    size = np.max([len(arr[i]) for i in range(k)])
+    masked = np.ma.empty((size,k))
+    masked.mask = True
+    for i in range(k):
+        masked[:len(arr[i]),i] = np.array(arr[i])
+    return masked
 
 if __name__ == "__main__":
     x = np.array([1/6.,2/6.,3/6.,1,.5,.1,.4])